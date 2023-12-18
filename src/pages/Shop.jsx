2f5f7e55b29da6e1a147b474550fae886f4c55d6
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 03:36:23 +0530

    Create Shop.jsx

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import ProductList from '../components/ProductList';
+import Section from '../components/UI/Section';
+
+const Shop = () => {
+  return (
+    <Section>
+      <ProductList />;
+    </Section>
+  );
+};
+
+export default Shop;
